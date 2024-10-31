584a9949d2ecc6039d73c551605a47a0a4c194d7
rafael bronzatti <rafaelbronzattig.ramos@gmail.com>
2024-10-30 22:05:39 -0300

    Uncommenting a few lines accidentaly commented out

--- main.py
+++ main.py
@@ -3,21 +3,21 @@ from src.DownloadFiles import DownloadFiles
 from src.CNPJDatabaseBuilder import CNPJDatabaseBuilder
 
 if __name__ == "__main__":
-    # # Base URL of the main directory
-    # base_url = "https://arquivos.receitafederal.gov.br/cnpj/dados_abertos_cnpj/"
+    # Base URL of the main directory
+    base_url = "https://arquivos.receitafederal.gov.br/cnpj/dados_abertos_cnpj/"
 
-    # # Initialize ExtractUrl to get the latest month's URL
-    # extractor = ExtractUrl(base_url)
+    # Initialize ExtractUrl to get the latest month's URL
+    extractor = ExtractUrl(base_url)
 
     try:
-        # latest_month_url = extractor.get_latest_month_url()
-        # print(f"Latest month URL: {latest_month_url}")
+        latest_month_url = extractor.get_latest_month_url()
+        print(f"Latest month URL: {latest_month_url}")
 
-        # # Initialize DownloadFiles to download the ZIP files with parallelism
-        # downloader = DownloadFiles(output_dir='output', max_workers=5)
+        # Initialize DownloadFiles to download the ZIP files with parallelism
+        downloader = DownloadFiles(output_dir='output', max_workers=5)
 
-        # # Download all ZIP files from the latest month's URL
-        # downloader.download_zip_files(latest_month_url)
+        # Download all ZIP files from the latest month's URL
+        downloader.download_zip_files(latest_month_url)
 
         # After downloading, build the database using CNPJDatabaseBuilder
         # Set the input_folder to 'output' where the ZIP files are stored
